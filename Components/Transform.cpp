73026eed10d992a16850cc7c33a5f1a16f2aa18b
Jarred Eagley <jarred.eagley@hotmail.com>
2020-11-04 17:52:46 -0800

    Updated transform component

--- Components/Transform.cpp
+++ Components/Transform.cpp
@@ -23,24 +23,42 @@
 
 Transform::Transform() : Component(ComponentTypes::TYPE_TRANSFORM) // Call the constructor of the base class with the correct type.
 {
-	mPositionX = mPositionY = 0.0f;
+	//mPositionX = mPositionY = 0.0f;
+	mPosition = glm::vec4(0, 0, 0, 1);
+	mRotation = 0;
+	mScale = glm::vec3(1, 1, 1);
+	mTransMatrix = glm::mat4(); // Identity.
 }
 
 Transform::~Transform()
 {}
 
 void Transform::Update()
-{} 
+{
+	buildTransformationMatrix();
+} 
+
+void Transform::buildTransformationMatrix()
+{
+	// Build the transformations.
+	glm::mat4 rot = glm::rotate( glm::mat4(),mRotation, glm::vec3(0,0,1) );
+	glm::mat4 trans = glm::translate( glm::mat4(), glm::vec3(mPosition) );
+	glm::mat4 scale = glm::scale( glm::mat4(), mScale );
+	// Build the transformation matrix.
+	mTransMatrix = glm::mat4() * trans * rot * scale;
+}
+
 
 void Transform::setX(float x)
 {
-	mPositionX = x;
+	mPosition.x = x;
 }
 
 void Transform::setY( float y)
 {
-	mPositionY = y;
+	mPosition.y = y;
 }
+
 
 void Transform::Serialize(rapidjson::Value::ConstMemberIterator inputMemberIt)
 {
--- Components/Transform.h
+++ Components/Transform.h
@@ -21,29 +21,36 @@
 #include "Component.h"
 #include "rapidjson/document.h"
 
-//class GameObject;
+#include "glm/glm.hpp" // Implement transformations!!!
+#include "glm/gtc/matrix_transform.hpp"
 
 class Transform : public Component 
 {
 public:
 	Transform();
 	~Transform();
+
 	void Update();
 
 	void setX(float x);
 	void setY(float y);
 
-	float getX() { return mPositionX; };
-	float getY() { return mPositionY; };
+	glm::vec3 getPosition() { return mPosition; };
+	float getRotation() { return mRotation; };
+	glm::mat4 getTransformationMatrix() { return mTransMatrix; };
 
-	void incrementX(float delta) { mPositionX += delta; }; // TO-DO: Maybe this should be in cpp...
-	void incrementY(float delta) { mPositionY += delta; };
+	void incrementX(float delta) { mPosition.x += delta; }; // TO-DO: Maybe this should be in cpp...
+	void incrementY(float delta) { mPosition.y += delta; };
+	void incrementRotation(float delta) { mRotation += delta; };
 	
 	virtual void Serialize(rapidjson::Value::ConstMemberIterator inputMemberIt);
 public:
 	// No public variables.
 private:
-	// No private methods.
+	void Transform::buildTransformationMatrix();
 private:
-	float mPositionX, mPositionY;
+	glm::vec4 mPosition;
+	float mRotation; // I'm not going to bother supporting 3D rotation.
+	glm::vec3 mScale;
+	glm::mat4 mTransMatrix;
 };
