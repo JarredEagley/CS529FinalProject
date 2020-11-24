a707919aa8a35ff01fa4cc58b4c655a502b87da5
Jarred Eagley <jarred.eagley@hotmail.com>
2020-11-23 19:20:27 -0800

    PhysicsManager.Update(), with broken iterators

--- Managers/PhysicsManager.cpp
+++ Managers/PhysicsManager.cpp
@@ -1,5 +1,6 @@
 #include "PhysicsManager.h"
 #include "GlobalManager.h"
+#include "../Components/ComponentTypes.h"
 #include "../Components/PhysicsBody.h"
 #include "../GameObject.h"
 
@@ -29,10 +30,36 @@ PhysicsManager::PhysicsManager()
 
 void PhysicsManager::Update(float frameTime)
 {
+	// Integrate the physics bodies.
 	for (auto pGOPair : GlobalManager::getGameObjectManager()->mGameObjects)
 	{
-		PhysicsBody* PhysicsBody = static_cast<PhysicsBody*>();
-		if (nullptr != PhysicsBody)
-			pBody->Integrate(frameTime);
+		PhysicsBody* pPhysicsBody = static_cast<PhysicsBody*>(pGOPair.second->GetComponent(ComponentTypes::TYPE_PHYSICSBODY));
+		if (nullptr != pPhysicsBody)
+			pPhysicsBody->Integrate(frameTime);
 	}
+
+	// Reset previous contacts.
+	GlobalManager::getCollisionManager()->Reset();
+
+	for (auto pObjPair1 = GlobalManager::getGameObjectManager()->mGameObjects.begin();
+		pObjPair1 != GlobalManager::getGameObjectManager()->mGameObjects.end();
+		++pObjPair1)
+	{
+		
+		PhysicsBody* pPhysicsBody1 = static_cast<PhysicsBody*>( (*pObjPair1)->second->GetComponent(ComponentTypes::TYPE_PHYSICSBODY) );
+		if (nullptr == pPhysicsBody1) continue;
+
+		for (auto pObjPair2 = pObjPair1 + 1;
+			pObjPair2 != GlobalManager::getGameObjectManager()->mGameObjects.end();
+			++pObjPair2)
+		{
+			PhysicsBody* pPhysicsBody2 = static_cast<PhysicsBody*>((*pObjPair2)->second->GetComponent(ComponentTypes::TYPE_PHYSICSBODY));
+			if (nullptr == pPhysicsBody2) continue;
+
+			// If we're here, we have two GO's with body components.
+			GlobalManager::getCollisionManager()->checkCollisionAndGenerateContact(pPhysicsBody1->mpShape, pPhysicsBody1->mPosition, pPhysicsBody2->mpShape, pPhysicsBody2->mPosition);
+		}
+	}
+
+	// TO-DO: Advanced physics resolutions go here.
 }
