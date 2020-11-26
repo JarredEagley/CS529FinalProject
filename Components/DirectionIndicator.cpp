de0c37e1d9d9110457e90ea4316544280b4e69c3
Jarred Eagley <jarred.eagley@hotmail.com>
2020-11-25 16:43:35 -0800

    Leaving off on directionIndicator to work on parenting

--- Components/DirectionIndicator.cpp
+++ Components/DirectionIndicator.cpp
@@ -1,1 +1,38 @@
+/* Start Header -------------------------------------------------------
+
+	Copyright (C) 20xx DigiPen Institute of Technology.
+	Reproduction or disclosure of this file or its contents without the
+	prior written consent of DigiPen Institute of Technology is prohibited.
+
+	File Name:			DirectionIndicator.cpp
+	Purpose				Implementations for DirectionIndicator.
+						Will interface with PhysicsBody and Transform.
+						May eventually track rotation angle as well.
+	Language:			C++, compiled using Microsoft Visual Studio 2019.
+	Platform:			Compiled using Visual Studio 2019, Windows 10.
+	Project:			JarredEagley_FinalProject
+	Author:				Jarred Eagley, jarred.eagley, SID: 400000520
+	Creation date:		11/25/2020
+
+- End Header --------------------------------------------------------*/
+
 #include "DirectionIndicator.h"
+#include "ComponentTypes.h"
+
+DirectionIndicator::DirectionIndicator() : Component(ComponentTypes::TYPE_UNDEFINED) // TO-DO
+{
+
+}
+
+DirectionIndicator::~DirectionIndicator()
+{
+
+}
+
+
+void DirectionIndicator::Update() 
+{
+
+}
+
+
--- Components/DirectionIndicator.h
+++ Components/DirectionIndicator.h
@@ -1,5 +1,39 @@
+/* Start Header -------------------------------------------------------
+
+	Copyright (C) 20xx DigiPen Institute of Technology.
+	Reproduction or disclosure of this file or its contents without the
+	prior written consent of DigiPen Institute of Technology is prohibited.
+
+	File Name:			DirectionIndicator.h
+	Purpose				A component for the direction indicators which appear
+						over ships and stations as the player zooms out.
+						This may eventually be used for prograde-vector as well
+						as ship-forward.
+						Will interface with PhysicsBody and Transform.
+						May eventually track rotation angle as well.
+	Language:			C++, compiled using Microsoft Visual Studio 2019.
+	Platform:			Compiled using Visual Studio 2019, Windows 10.
+	Project:			JarredEagley_FinalProject
+	Author:				Jarred Eagley, jarred.eagley, SID: 400000520
+	Creation date:		11/25/2020
+
+- End Header --------------------------------------------------------*/
+
 #pragma once
-class DirectionIndicator
+#include "Component.h"
+
+
+class DirectionIndicator : public Component
 {
+public:
+	DirectionIndicator();
+	~DirectionIndicator();
+
+	void Update();
+
+
+public:
+private:
+private:
 };
 
