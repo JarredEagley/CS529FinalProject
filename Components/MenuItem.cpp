f7c2cab10c7a811b2dccbb53f6688a2bd804fa6a
Jarred Eagley <jarred.eagley@hotmail.com>
2020-12-08 21:01:19 -0800

    header update

--- Components/MenuItem.cpp
+++ Components/MenuItem.cpp
@@ -4,11 +4,8 @@
 	Reproduction or disclosure of this file or its contents without the
 	prior written consent of DigiPen Institute of Technology is prohibited.
 
-	File Name:			MenuItem.h
-	Purpose				Menu components will be used to build the majority
-						of menu structures in the game. Clicking on them
-						broadcasts an event for the specific item.
-						Each menu item will be a game object.
+	File Name:			MenuItem.c[[
+	Purpose				Implementations for MenuItem.h
 	Language:			C++, compiled using Microsoft Visual Studio 2019.
 	Platform:			Compiled using Visual Studio 2019, Windows 10.
 	Project:			JarredEagley_FinalProject
