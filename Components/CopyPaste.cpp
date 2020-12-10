7f9ead1c3a5f44db5af4329461b62933a5c87a55
Jarred Eagley <jarred.eagley@hotmail.com>
2020-12-09 20:45:54 -0800

    I would say class template but thats a different thing
    copy paste to easily make new components

---
+++ Components/CopyPaste.cpp
@@ -0,0 +1,32 @@
+
+
+#include "CopyPaste.h"
+
+CopyPaste::CopyPaste() : Component(ComponentTypes::TYPE_UNDEFINED)
+{
+}
+
+CopyPaste::~CopyPaste()
+{
+}
+
+
+void CopyPaste::Initialize()
+{
+}
+
+void CopyPaste::Update()
+{
+}
+
+void CopyPaste::handleEvent(Event* pEvent)
+{
+}
+
+
+void CopyPaste::Serialize(rapidjson::Value::ConstMemberIterator inputMemberIt)
+{
+
+
+
+}
