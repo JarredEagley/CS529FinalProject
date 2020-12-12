0d9b08551c3e686cea4a5cd2f619015fbc543e5d
jarred.eagley <jarred.eagley@gmail.com>
2020-12-11 17:23:51 -0800

    beginning of ai generalization

---
+++ Components/AIEnemyCore.cpp
@@ -0,0 +1,31 @@
+
+#include "AIEnemyCore.h"
+
+AIEnemyCore::AIEnemyCore() : Component(ComponentTypes::TYPE_UNDEFINED)
+{
+}
+
+AIEnemyCore::~AIEnemyCore()
+{
+}
+
+
+void AIEnemyCore::Initialize()
+{
+}
+
+void AIEnemyCore::Update()
+{
+}
+
+void AIEnemyCore::handleEvent(Event* pEvent)
+{
+}
+
+
+void AIEnemyCore::Serialize(rapidjson::Value::ConstMemberIterator inputMemberIt)
+{
+
+
+
+}
---
+++ Components/AIEnemyCore.h
@@ -0,0 +1,32 @@
+///
+/// DELETE ME IN THE FINAL PRODUCT.
+/// 
+
+#pragma once
+#include "Component.h"
+#include "ComponentTypes.h"
+
+class AIEnemyCore : public Component
+{
+public:
+	AIEnemyCore();
+	~AIEnemyCore();
+
+	void Initialize();
+	void Update();
+	void handleEvent(Event* pEvent);
+
+	// Used for components which draw to openGL.
+	void setUniformData(ShaderProgram* pProgram) {};
+	void Draw(ShaderProgram* pProgram, glm::mat4 modelTrans, glm::mat4 viewTrans, glm::mat4 viewProj) {};
+
+	void Serialize(rapidjson::Value::ConstMemberIterator inputMemberIt);
+
+public:
+	// No public variables.
+private:
+	// No private methods.
+private:
+	// No private variables.
+};
+
