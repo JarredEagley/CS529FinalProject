0b959a35bd4d0958d0c9a7cc71ab23128028911e
jarred.eagley <jarred.eagley@gmail.com>
2020-12-11 17:48:43 -0800

    beginning of navigational line

---
+++ Components/OrbitalLine.cpp
@@ -0,0 +1,32 @@
+
+
+#include "CopyPaste.h"
+
+OrbitalLine::OrbitalLine() : Component(ComponentTypes::TYPE_UNDEFINED)
+{
+}
+
+OrbitalLine::~OrbitalLine()
+{
+}
+
+
+void OrbitalLine::Initialize()
+{
+}
+
+void OrbitalLine::Update()
+{
+}
+
+void OrbitalLine::handleEvent(Event* pEvent)
+{
+}
+
+
+void OrbitalLine::Serialize(rapidjson::Value::ConstMemberIterator inputMemberIt)
+{
+
+
+
+}
